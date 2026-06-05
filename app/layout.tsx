7c694e26081a990595e2c1be3f38051f0d7e2c6e
yasir hussain <yasir@highermarketingplus.com>
2026-06-05 13:00:50 +0500

    tawk to agent added

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,19 @@ export default async function RootLayout({ children }: Readonly<{ children: Reac
         </a>
         <main id="main-content">{children}</main>
         <Script src="/hmp-widget.js" strategy="afterInteractive" />
+        <Script id="tawk-to-chat" strategy="afterInteractive">
+          {`
+            var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+            (function() {
+              var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+              s1.async = true;
+              s1.src = "https://embed.tawk.to/6a2275f418ebf31c2bb3eff9/1jqb9pgdg";
+              s1.charset = "UTF-8";
+              s1.setAttribute("crossorigin", "*");
+              s0.parentNode.insertBefore(s1, s0);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   );
